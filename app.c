757b1e933a48003048373f317bb60485b21df2fb
karnjye.lau <karnjye.lau@kontron.com>
2020-11-25 15:05:57 +0800

    MLD app.c: added more comment and rename structural parameter for details clarification.

--- app.c
+++ app.c
@@ -1,48 +1,67 @@
+#include <memory.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
+#include "app.h"
 
-#define OFFSETOF(struct_name, fld_name)     \
-    (unsigned long)&(((struct_name *)0)->fld_name)
+int add_structure_to_struct_db(struct_db_t *struct_db, 
+                               struct_db_rec_t *struct_rec)
+{
 
-#define FIELD_SIZE(struct_name, fld_name)   \
-    sizeof(((struct_name *)0)->fld_name)
+    /*Note #struct_db is from  main() and 
+    #struct_rec is create dynamically from REG_STRUCT */
+    
+    struct_db_rec_t *head = struct_db->head;
 
-#define MAX_FIELD_NAME_SIZE 128
+    if(head == NULL)
+    {
+        struct_db->head = struct_rec;
+        struct_rec->next = NULL;
+        struct_db->count++;
+        return 0;
+    }
 
-#define FIELD_INFO(struct_name, fld_name, dtype, nested_struct_name)    \
-   {#fld_name, dtype, FIELD_SIZE(struct_name, fld_name),                \
-        OFFSETOF(struct_name, fld_name), #nested_struct_name} 
+    struct_rec->next = head;
+    struct_db->head = struct_rec;
+    struct_db->count++;
+    return 0;
+}
 
-typedef enum {
-    UINT8,
-    UINT32,
-    INT32,
-    CHAR,
-    OBJ_PTR,
-    VOID_PTR, /*New Data type added to identify void * pointers*/
-    FLOAT,
-    DOUBLE,
-    OBJ_STRUCT
-} data_type_t;
-
-typedef struct _field_info_{
-    char fname [MAX_FIELD_NAME_SIZE];   /*Name of the field*/
-    data_type_t dtype;                  /*Data type of the field*/
-    unsigned int size;                  /*Size of the field*/
-    unsigned int offset;                /*Offset of the field*/
-    // Below field is meaningful only if dtype = OBJ_PTR, Or OBJ_STRUCT
-    char nested_str_name[MAX_FIELD_NAME_SIZE];
-} field_info_t;
+void print_structure_rec(struct_db_rec_t *struct_rec){
+     printf("print_structure_rec\n");
+    if(!struct_rec) return;
+    int j = 0;
+    field_info_t *field = NULL;
+    printf(ANSI_COLOR_CYAN "|------------------------------------------------------|\n" ANSI_COLOR_RESET);
+    printf(ANSI_COLOR_YELLOW "| %-20s | size = %-8d | #flds = %-3d |\n" ANSI_COLOR_RESET, struct_rec->struct_name, struct_rec->ds_size, struct_rec->n_fields);
+    printf(ANSI_COLOR_CYAN "|------------------------------------------------------|------------------------------------------------------------------------------------------|\n" ANSI_COLOR_RESET);
+    for(j = 0; j < struct_rec->n_fields; j++){
+        field = &struct_rec->fields[j];
+        printf("  %-20s |", "");
+        printf("%-3d %-20s | dtype = %-15s | size = %-5d | offset = %-6d|  nstructname = %-20s  |\n",j, field->fname, DATA_TYPE[field->dtype], field->size, field->offset, field->nested_str_name);
+        printf("  %-20s |", "");
+        printf(ANSI_COLOR_CYAN "--------------------------------------------------------------------------------------------------------------------------|\n" ANSI_COLOR_RESET);
+    }
+}
 
 
-typedef struct emp_ {
-
-    char emp_name[30];
-    unsigned int emp_id;
-    unsigned int age;
-    struct emp_ *mgr;
-    float salary;
-    int *p;
-} emp_t;
+void print_structure_db(struct_db_t *struct_db){
+    
+    printf("print_structure_db\n");
+    if(struct_db != NULL) return;
+    
+    printf("printing STRUCURE DATABASE\n");
+    int i = 0;
+    struct_db_rec_t *struct_rec = NULL;
+    struct_rec = struct_db->head;
+    printf("No of Structures Registered = %d\n", struct_db->count);
+    while(struct_rec)
+    {
+        printf("structure No : %d (%p)\n", i++, struct_rec);
+        print_structure_rec(struct_rec);
+        struct_rec = struct_rec->next;
+    }
+}
 
 int  main(int argc, char* argv[])
 {
@@ -57,11 +76,17 @@ int  main(int argc, char* argv[])
         FIELD_INFO(emp_t, p, OBJ_PTR, 0)
     };
 
-    printf("filed name is %s\n",emp_fields[1].fname);
-    printf("size is %d\n",emp_fields[1].size);
-    printf("offset is %d\n",emp_fields[0].offset);
+    struct_db_t *struct_db = calloc(1, sizeof(struct_db_t));
 
+    if(struct_db->head == NULL)
+    {
+        printf("struct_db->head is NULL\n");
+    }
 
-return 0;
+    REG_STRUCT(struct_db, emp_t, emp_fields);
+    print_structure_rec(struct_db->head);
+
+      //  print_structure_db(struct_db);
+    return 0;
 }
 
